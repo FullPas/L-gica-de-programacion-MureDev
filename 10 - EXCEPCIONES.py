d1aa8b1ea38f47b2aa525b81684eeec7e3ccfedb
FullPas <cse@hotmail.es>
2024-09-24 12:39:42 +0200

    Ejercicio 10 (Extra)

--- 10 - EXCEPCIONES.py
+++ 10 - EXCEPCIONES.py
@@ -1,3 +1,21 @@
+"""
+ * EJERCICIO:
+ * Explora el concepto de manejo de excepciones según tu lenguaje.
+ * Fuerza un error en tu código, captura el error, imprime dicho error
+ * y evita que el programa se detenga de manera inesperada.
+ * Prueba a dividir "10/0" o acceder a un índice no existente
+ * de un listado para intentar provocar un error.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una función que sea capaz de procesar parámetros, pero que también
+ * pueda lanzar 3 tipos diferentes de excepciones (una de ellas tiene que
+ * corresponderse con un tipo de excepción creada por nosotros de manera
+ * personalizada, y debe ser lanzada de manera manual) en caso de error.
+ * - Captura todas las excepciones desde el lugar donde llamas a la función.
+ * - Imprime el tipo de error.
+ * - Imprime si no se ha producido ningún error.
+ * - Imprime que la ejecución ha finalizado. 
+"""
 
 try:
     print(10/1)
@@ -5,9 +23,43 @@ try:
 except Exception as e:
     print(f"Se ha producido un error: {e}")
 
-#EXTRA
+#EXTRA                                                          #Se va a generar de forma abrupta un Index Error, en la consulta d eun tercer elemento de una lista ( que no existe)
+
+
+class StrTypeError(Exception):                                  #Error personalizado
+    pass                                                        #Sin más pasa.                                                                
 
 def process_params(parameters: list):
-    print(parameters[2])
+    if len(parameters) < 3:
+        raise IndexError()                                      #Raise es una palabra reservada para controlar y lanzar el tipo de error, en este caso por consola.
+    elif parameters[1] == 0:                                    #Aquí vamos a provocar un erorr que hará que se detenga el programa.
+        raise ZeroDivisionError()                               #Con ésto lo capturamos para controlarlo.
+    
+    elif type(parameters[2]) == str:
+        raise StrTypeError ("El segundo elemento de la lista no puede ser una cadena de texto")
+        
+    print(parameters[2])                                                #Nosotros aquí buscamos acceder al parámetro de la posición 2 de la lista, pero... y su dividimos 0, en tre 1? nos dará error?
+    print(parameters[0]/parameters[1])                                  #El aprámetro 0 , lo dividimos entre el parámetro 1
+    print(parameters[2] +5)
+#Ahora, capturemos el error...
 
-    process_params([1, 2, 3])
+#----------------------------------------------------------------------------------------------------------------------------------------------
+# 2 Errores del sistema
+try:
+    process_params([1, 2, 3])                                           #si añadimos un 0 en la 2ª posicion (dará ZeroDivisionError), Si sustituimos el 3 por "brais" (dará error inesperado)
+except IndexError as e:
+    print("El número de la lista debe ser mayor que dos")
+except ZeroDivisionError as e:
+    print("El segundo elemento de la lista no puede ser un cero")       #Pues quí estamos capturando el error que generaría el programa al intentar dividir el elemento 2 de la lista.
+#------------------------------------------------------------------------------------------------------------------------------------------------
+# 1 Error personalizado
+except StrTypeError as e:
+    print(f"{e}")
+
+#Un Error de descarte
+
+except Exception as e:                                                   #Como buena práctica, si no entra en ningun bloque anterior, que pueda salir por el siguiente:
+    print(f"Se ha producido un error inesperado {e}")
+print("El programa finaliza")
+
+
